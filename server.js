11174519ee064d2eb01cb481d0fff8d4bccaf078
MiracleMeds <161770009+MiracleMeds@users.noreply.github.com>
2024-04-14 15:24:45 -0400

    added extra comments on server.js

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 //Don't move me!
-//Open server with npm run dev
+//Open server with npm run dev (dev= 'nodemon server')
+//using expressjs for my website.
 const express = require('express');
 const site = express();
 const path = require('path');
 const PORT = process.env.PORT || 8000;
 
 //middleware used to handle url-encoded data (forum data)
-// 'content-type: application/x-www-form-urlencoded'
+// 'content-type: application/x-www-form-urlencoded'. Unknown if I will need it, but hey, might as well.
 site.use(express.urlencoded({extended: false}));
 
-//middleware for json files
+//middleware for json files. I doubt I need it, but just in case.
 site.use(express.json());
 
-//serve static files (like css)
+//serve static files (like css). Allows easy access to linked stylesheets/scripts/photos to the website without having to
+//write redundant code
 site.use('/public', express.static(path.join(__dirname, '/public')));
 
+//Allows the server to understand what each link means when a request to move to a page is done.
+// ^ = nothing before, $ = nothing after, ()? = optional.
 site.get('^/$|/index(.html)?|/home( page)?', (req, res) =>{
+    //go to this file. Start looking at ___dirname (the root file for this project) and go to'index.html'
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 site.get('/ActionList(.html)?|/action(list)?|/actions', (req, res) =>{
